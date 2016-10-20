594d87fdec27194f90b2ab49e5a872d093138134
emag <lanabe.lanabe@gmail.com>
2016-10-21 01:01:46 +0900

    Update setup.py

--- python-professional-programming/ch03/setup.py
+++ python-professional-programming/ch03/setup.py
@@ -1,8 +1,20 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
 
 setup(
     name='guestbook',
     version='1.0.0',
+    description='A guestbook web application.',
+    long_description=read_file('README.rst'),
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
